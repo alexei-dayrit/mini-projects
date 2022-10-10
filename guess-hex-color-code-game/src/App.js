b7dd5a0beb53f239d0755d280189bc96857016c6
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-10-10 00:29:09 +0000

    Formatted basic structure and styling of app.

--- guess-hex-color-code-game/src/App.js
+++ guess-hex-color-code-game/src/App.js
@@ -2,9 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
+    <section className="container">
+      <h1 className="col-full">Hex Color Code Game</h1>
 
-    </div>
+      <div className="flex-center">
+        <div className="col-full">
+          <div className="color-box">FULL COLOR BOX</div>
+        </div>
+        <h2 className="col-full">Take your guess:</h2>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+    </section>
   );
 }
 
