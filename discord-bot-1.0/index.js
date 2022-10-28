dd5d74a3695eecce77370c8e71e05dad7c4322f2
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-10-27 18:39:33 -0700

    Added watch script into package.json and destructured import from discord.js instead.

--- discord-bot-1.0/index.js
+++ discord-bot-1.0/index.js
@@ -1,11 +1,20 @@
-import Discord from 'discord.js';
+import { Client } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const client = new Discord.Client({ intents: ['Guilds', 'GuildMessages'] });
+const client = new Client({
+  intents: [
+    'Guilds',
+    'GuildMessages'
+  ]
+});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('messageCreate', msg => {
+  console.log(msg.content);
 });
 
 client.login(process.env.TOKEN);
