b22b3975c208c787936820ef2fa709c6b1b12ea4
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-10-30 17:15:18 -0700

    Imported Routes from discord.js. Added global vars for CLIENT_ID and GUILD_ID.

--- discord-bot-1.0/index.js
+++ discord-bot-1.0/index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, Routes, GatewayIntentBits } from 'discord.js';
 import { REST } from '@discordjs/rest';
 
 dotenv.config();
 
 const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({
   intents: [
@@ -16,9 +18,16 @@ const client = new Client({
 
 const rest = new REST({ version: 10 }).setToken(TOKEN);
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
-});
+client.on('ready', () => console.log(`Logged in as ${client.user.username}!`));
+
+const main = async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID), GUILD_ID);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 client.on('messageCreate', msg => {
   console.log(msg.author.tag);
