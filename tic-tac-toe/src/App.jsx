3754edcb51df11231fbea3f12fdc88dbd1d22ed9
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-01 17:39:48 -0700

    Created tic tac toe board. Added 100vh to root and centered all items onto screen.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,8 +4,26 @@ import './App.css';
 function App() {
 
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <div className='bg-orange-400 w-32 h-32'></div>
+    <div className='w-[500px] flex justify-center gap-2 flex-wrap border-2 border-black'>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+      <div className='w-32 h-32 border-2 border-black'></div>
+
+      {/* <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
+      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div> */}
     </div>
   );
 }
