dc988f261abe0a9e22d46f1e4324fc3f75189313
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-01 22:04:00 -0700

    Added currentPlayer state to Board.jsx and added tileValue state to Tile.jsx. Also created a handleClick function for tiles to change it's text content.

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -5,11 +5,12 @@ const INITIAL_GAME_STATE = [null, null, null, null, null, null, null, null, null
 
 const Board = () => {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
 
   return (
     <div className='w-[300px] sm:w-[450px] flex justify-center items-center gap-3 flex-wrap border-2 border-gray-700 p-4'>
       {gameState.map((tile, index) => (
-        <Tile key={index}/>
+        <Tile key={index} currentPlayer={currentPlayer}/>
       ))}
     </div>
   );
--- tic-tac-toe/src/components/Tile.jsx
+++ tic-tac-toe/src/components/Tile.jsx
@@ -1,6 +1,24 @@
-const Tile = () => {
+import { useState } from 'react';
+
+const Tile = ({ currentPlayer }) => {
+  const [tileValue, setTileValue] = useState('');
+
+  function handleClick() {
+    console.log('currentPlayer', currentPlayer);
+    if (currentPlayer === 'X') {
+      setTileValue('X');
+    } else {
+      setTileValue('O');
+    }
+  }
+
   return (
-    <div className='w-20 h-20 sm:w-32 sm:h-32 border-2 border-white'></div>
+    <div
+      onClick={handleClick}
+      className='w-20 h-20 sm:w-32 sm:h-32 border-2 border-white'>
+      {tileValue}
+    </div>
+
   );
 };
 
