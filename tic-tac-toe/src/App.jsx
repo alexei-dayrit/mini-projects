b2b416363d2368746342ed63d79b92b64fcbf1ab
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-01 17:56:45 -0700

    Updated sizing and added media queries to be mobile responsive.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,21 +4,21 @@ import './App.css';
 function App() {
 
   return (
-    <div className='w-[450px] flex justify-center items-center gap-3 flex-wrap border-2 border-gray-700 p-4'>
+    <div className='w-[300px] sm:w-[450px] flex justify-center items-center gap-3 flex-wrap border-2 border-gray-700 p-4'>
       <div className='flex gap-3 w-full'>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
       </div>
       <div className='flex gap-3 w-full'>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
       </div>
       <div className='flex gap-3 w-full'>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
-        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
+        <div className='w-1/3 h-24 sm:h-32 border-2 border-white'></div>
       </div>
     </div>
   );
