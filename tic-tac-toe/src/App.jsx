a2f1e5d6695049156631d8e010c26917add9627a
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-01 17:50:29 -0700

    Wrapped each row of tiles into a div. Adjusted colors.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,26 +4,22 @@ import './App.css';
 function App() {
 
   return (
-    <div className='w-[500px] flex justify-center gap-2 flex-wrap border-2 border-black'>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-      <div className='w-32 h-32 border-2 border-black'></div>
-
-      {/* <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div>
-      <div className='w-32 h-32 bg-orange-400 border-2 border-black'></div> */}
+    <div className='w-[450px] flex justify-center items-center gap-3 flex-wrap border-2 border-gray-700 p-4'>
+      <div className='flex gap-3 w-full'>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+      </div>
+      <div className='flex gap-3 w-full'>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+      </div>
+      <div className='flex gap-3 w-full'>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+        <div className='w-32 h-32 border-2 border-white'></div>
+      </div>
     </div>
   );
 }
