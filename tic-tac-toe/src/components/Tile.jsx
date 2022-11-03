65b65c1095156a6ff485e8be54f96357e45d4023
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-02 17:46:57 -0700

    Added data-tile-id attribute to tiles to keep track of which one was clicked.

--- tic-tac-toe/src/components/Tile.jsx
+++ tic-tac-toe/src/components/Tile.jsx
@@ -1,9 +1,12 @@
-const Tile = ({ tile, handleTileClick }) => {
+const Tile = ({ tile, tileId, handleTileClick }) => {
   return (
     <div
       onClick={handleTileClick}
+      data-tile-id={tileId}
       className='w-20 h-20 sm:w-32 sm:h-32 flex border-2 border-white'>
-      <h1 className="w-full text-4xl sm:text-7xl flex justify-center items-center">{tile}</h1>
+      <h1 className="w-full text-4xl sm:text-7xl flex justify-center items-center">
+        {tile}
+      </h1>
     </div>
 
   );
