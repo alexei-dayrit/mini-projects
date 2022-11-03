d1ad893d9ea01761cb162d04e046e50f82b72881
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-02 17:40:36 -0700

    Moved tile click function into board.jsx to avoid passing numerous props.

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -7,11 +7,29 @@ const Board = () => {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   const [currentPlayer, setCurrentPlayer] = useState('X');
 
+  function handleTileClick(event) {
+    const id = event.target.getAttribute('key');
+    console.log('id:', id);
+    const test = gameState.concat([]);
+    console.log(test);
+    test[id] = currentPlayer;
+    console.log(test);
+    setGameState(test);
+    if (currentPlayer === 'X') {
+      console.log('x');
+    }
+  }
+
   return (
     <div className='w-[300px] sm:w-[450px] flex justify-center items-center gap-3 flex-wrap border-2 border-gray-700 p-4'>
-      {gameState.map((tile, index) => (
-        <Tile key={index} currentPlayer={currentPlayer}/>
-      ))}
+      {gameState.map((tile, index) => {
+        console.log('tile map:', tile);
+        return (<Tile
+          key={index}
+          data-tile-id={index}
+          {...{ handleTileClick, tile }}
+        />);
+      })}
     </div>
   );
 };
--- tic-tac-toe/src/components/Tile.jsx
+++ tic-tac-toe/src/components/Tile.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react';
-
-const Tile = ({ currentPlayer }) => {
-  const [tileValue, setTileValue] = useState('');
-
-  function handleClick() {
-    console.log('currentPlayer', currentPlayer);
-    if (currentPlayer === 'X') {
-      setTileValue('X');
-    } else {
-      setTileValue('O');
-    }
-  }
-
+const Tile = ({ tile, handleTileClick }) => {
   return (
     <div
-      onClick={handleClick}
+      onClick={handleTileClick}
       className='w-20 h-20 sm:w-32 sm:h-32 flex border-2 border-white'>
-      <h1 className="w-full text-4xl sm:text-7xl flex justify-center items-center">{tileValue}</h1>
+      <h1 className="w-full text-4xl sm:text-7xl flex justify-center items-center">{tile}</h1>
     </div>
 
   );
