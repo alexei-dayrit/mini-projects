398cdceb20cc29d629dae58bb87c73bcc1f9f0d7
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-10 16:54:22 -0800

    Created sudoku grid with css grid and map().

--- sudoku/src/App.jsx
+++ sudoku/src/App.jsx
@@ -2,10 +2,30 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sudokuGrid, setSudokuGrid] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]);
 
   return (
     <div className="App">
       <h1>Sudoku</h1>
+      <div className='grid'>
+        {sudokuGrid.map((col, colIndex) => (
+          <div key={colIndex} className='col'>
+            {col.map((cell, cellIndex) => (
+              <div key={cellIndex} className='cell'>{cell}</div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
