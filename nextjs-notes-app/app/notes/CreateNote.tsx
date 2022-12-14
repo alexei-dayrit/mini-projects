76b323c56fb19faa551526a833545dca03b625fc
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-12-14 03:35:00 +0000

    Imported useState and created state for title and content.

--- nextjs-notes-app/app/notes/CreateNote.tsx
+++ nextjs-notes-app/app/notes/CreateNote.tsx
@@ -1,6 +1,11 @@
 'use client'
 
+import { useState } from 'react';
+
 export default function CreateNote() {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
   return (
     <div>
       <h1>Create Note</h1>
