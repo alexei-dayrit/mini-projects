0fd7dbaed9601bf5364fd45088db7e3b58f79977
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-12-14 02:42:01 +0000

    Exported variables to change behavior since not using fetch.

--- nextjs-notes-app/app/notes/page.tsx
+++ nextjs-notes-app/app/notes/page.tsx
@@ -1,18 +1,27 @@
 import PocketBase from 'pocketbase';
 
+export const dynamic = 'auto',
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = 'auto',
+  runtime = 'nodejs',
+  preferredRegion = 'auto';
+
+console.log(dynamicParams)
+
 async function getNotes() {
   const db = new PocketBase('http://127.0.0.1:8090');
   const records = await db.collection('notes').getList(1, 25);
-  return records?.items as any[]
+  return records?.items as any[];
 }
 
 export default async function NotesPage() {
-  const notes = await getNotes()
+  const notes = await getNotes();
   return (
     <div>
       <h1>Notes Page</h1>
       <div>
-        {notes.map((note) => (
+        {notes.map(note => (
           <div key={note.id}>
             <h2>{note.title}</h2>
             <p>{note.content}</p>
